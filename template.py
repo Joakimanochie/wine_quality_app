d4ac6bd80008c704e85aace2100a970e3394c275
Joakimanochie <anochietobe@gmail.com>
2024-04-29 17:41:03 +0100

    model experiment

--- template.py
+++ template.py
@@ -2,12 +2,13 @@ import os
 from pathlib import Path
 import logging
 
+#logging structure
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 project_name = "winequality"
 
 
-
+#list of file to create
 list_of_files = [
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
@@ -36,12 +37,15 @@ list_of_files = [
 for filepath in list_of_files:
     filepath = Path(filepath)
 
+    # Spliting file name and directory
     filedir, filename = os.path.split(filepath)
 
+    # checks if directory name is not empty then creates it
     if filedir !="":
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory; {filedir} for the file: {filename}")
     
+    # checking if file exists in the path and checks the file size, then creates it
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass
